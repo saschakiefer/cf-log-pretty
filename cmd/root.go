4274abd581c053d24353d2f4731f27fb5a3190b1
Sascha Kiefer <sascha.kiefer@sap.com>
2026-01-28 13:49:16 +0100

    Change default log level to TRACE in command line flags

--- cmd/root.go
+++ cmd/root.go
@@ -48,7 +48,7 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&cfg.Level, "level", "l", "DEBUG", "minimum log level to include (TRACE, DEBUG, INFO, WARN, ERROR)")
+	rootCmd.Flags().StringVarP(&cfg.Level, "level", "l", "TRACE", "minimum log level to include (TRACE, DEBUG, INFO, WARN, ERROR)")
 	rootCmd.Flags().StringVarP(&cfg.RemovePrefix, "remove-logger-prefix", "r", "", "remove given prefix from logger names (e.g. \"com.foo.prod.\")")
 	rootCmd.Flags().StringSliceVarP(&cfg.Exclude, "exclude-logger", "e", []string{}, "exclude logs from given loggers. Supports exact match (e.g. \"com.foo.Service\") or package wildcard (e.g. \"com.foo.core.*\" for packages and sub-packages)")
 	rootCmd.Flags().BoolVarP(&cfg.TruncateRaw, "truncate-raw", "t", false, "truncate raw log messages to terminal width (if message is not in JSON format, e.g. platform logs)")
